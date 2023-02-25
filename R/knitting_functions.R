1b219d589a594a4a2ac919dcf0aa747e4bce8e05
RainmanMathers <janis.sonnemann@gmail.com>
2023-02-25 18:45:37 +0100

    Changed Knitting function to allow specification of desired output format

--- R/knitting_functions.R
+++ R/knitting_functions.R
@@ -2,20 +2,21 @@
 #'
 #' @param input unchanged
 #' @param path change to name of desired subdirectory
+#' @param format specify desired files to output in quotes: "html_document", "pdf_document", "all"
 #' @param ... further arguments
 #'
 #' @return
 #' @export
 #'
 #' @examples
-knit_multiple_dated <- function(input, path, ...) {
+knit_multiple_dated <- function(input, path, format, ...) {
   rmarkdown::render(
     input,
     output_dir = paste0("doc/output/", path),
     output_file = paste0(
       xfun::sans_ext(input), '-', Sys.Date()
     ),
-    output_format = "all",
+    output_format = format,
     envir = globalenv()
   )
 }
