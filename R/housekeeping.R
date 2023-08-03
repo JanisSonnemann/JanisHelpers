9d6fd43327d15d469cbf500ffbb5742fcf5a65b8
Janis Sonnemann <45274761+JanisSonnemann@users.noreply.github.com>
2023-08-03 18:18:38 +0200

    update to description

--- R/housekeeping.R
+++ R/housekeeping.R
@@ -8,6 +8,12 @@ update_JanisHelpers <- function(){
   devtools::install("C:/Users/janis/Documents/R/eigene R Pakete/JanisHelpers")
 }
 
+#' update_JanisHelpers_git: update from private git repo
+#'
+#' @return
+#' @export
+#'
+#' @examples
 update_JanisHelpers_git <- function() {
   devtools::install_github("JanisSonnemann/JanisHelpers", auth_token = gh::gh_token())
 }
