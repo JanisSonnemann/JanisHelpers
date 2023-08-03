24025cd49f49952ec07159ca5b9b6207cf6b9ac8
Janis Sonnemann <45274761+JanisSonnemann@users.noreply.github.com>
2023-08-03 18:12:14 +0200

    Included update function to update from private git repo

--- R/housekeeping.R
+++ R/housekeeping.R
@@ -7,3 +7,8 @@
 update_JanisHelpers <- function(){
   devtools::install("C:/Users/janis/Documents/R/eigene R Pakete/JanisHelpers")
 }
+
+update_JanisHelpers_git <- function() {
+  devtools::install_github("JanisSonnemann/JanisHelpers", auth_token = gh::gh_token())
+}
+
