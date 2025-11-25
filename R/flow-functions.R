1971e102754ce2beba72c5cf7f26b41b11731967
Janis Sonnemann <45274761+JanisSonnemann@users.noreply.github.com>
2025-11-25 22:06:22 +0100

    Created new function to export data from FlowJo in long format

--- R/flow-functions.R
+++ R/flow-functions.R
@@ -189,3 +189,124 @@ import_fcs <- function(clean, path, group, r_stats, keywords) {
   data |>
     dplyr::tibble()
 }
+
+
+#' Import FACS data from FlowJo workspace in long format
+#'
+#' @param path path to .wsp file
+#' @param group group to be extracted from workspace default = NULL
+#' @param r_stats true or false to indicate if statistics such as MFI should be exported
+#' @param keywords character vector of keywords to be exported, default = NULL
+#'
+#' @returns
+#' @export
+#'
+#' @examples
+import_workspace_long <- function(path, group = NULL, r_stats = FALSE, keywords = NULL) {
+
+  # Import raw workspace
+  ps_raw <- suppressMessages(fcexpr::wsx_get_popstats(ws = path, return_stats = r_stats, groups = group))
+
+  ## extract counts
+  counts <- ps_raw[["counts"]] |>
+    select(FileName, PopulationFullPath, Population, Count, FractionOfParent) |>
+    pivot_longer(
+      cols = c(Count, FractionOfParent),
+      names_to = "metric",
+      values_to = "value"
+    )
+
+  # Import keywords
+  keys <- fcexpr::wsx_get_keywords(ws = path, return = "data.frame") |>
+    enframe() |>
+    rename(
+      FileName = name
+    ) |>
+    unnest(c(FileName, value)) |>
+    rename(key = name)
+
+  # If stats should be extracted: merge stats and counts
+  if (r_stats == TRUE) {
+
+    ## Extract channel labels from keywords
+    stains <- keys |>
+      filter(grepl("\\$P[0-9]+[NS]", key)) |>
+      mutate(
+        type = case_when(
+          grepl("\\$P[0-9]+N", key) ~ "channel",
+          grepl("\\$P[0-9]+S", key) ~ "stain"
+        ),
+        number = str_extract(key, "(?<=P)[0-9]+")
+      ) |>
+      select(!key) |>
+      pivot_wider(
+        names_from  = type,
+        values_from = value
+      ) |>
+      select(!number)
+
+    ## subset stats from list
+    stats <- ps_raw[["stats"]] |>
+      ## add channel labels
+      left_join(
+        y = stains,
+        by = c("FileName", "channel")
+      ) |>
+      ## create new column which combined statistic name and channel label
+      mutate(
+        stain = ifelse(stain == "", NA, stain),
+        metric = ifelse(!is.na(statistic), paste0(statistic, "_", stain), NA),
+        Population = basename(PopulationFullPath)
+      ) |>
+      select(FileName, PopulationFullPath, Population, metric, value)
+
+    # combined counts and stats
+    df <- bind_rows(
+      counts,
+      stats
+    )
+
+  }
+  # Return only counts if stats are not extracted
+  else {
+
+    # only take counts from list
+    df <- counts
+
+  }
+  # extract selected keywords and pivot to wide
+  keys_clean <- keys |>
+    filter(key %in% c(keywords)) |>
+    pivot_wider(
+      names_from = key,
+      values_from = value
+    )
+
+  # Merge workspace data with keywords
+  final <- left_join(df, keys_clean, by = "FileName") |>
+    arrange(FileName, PopulationFullPath) |>
+    relocate(mouse_ID, tissue, PopulationFullPath, Population, metric, value)
+
+  # create summary to make sure every thing was exported correctly
+  n_files <- n_distinct(final$FileName)
+  n_mice <- n_distinct(final$mouse_ID)
+  n_tissues <- n_distinct(final$tissue)
+
+  summary_text <- glue::glue(
+    "
+Extraction Summary
+----------------------------------------------
+Extracted groups:          {group}
+Number of samples:         {n_files}
+Unique mouse_IDs:          {n_mice}
+Unique tissues:            {n_tissues}
+----------------------------------------------
+"
+  )
+  ## print summary message
+  message(summary_text)
+
+  # return final data invisibly
+  invisible(final)
+
+}
